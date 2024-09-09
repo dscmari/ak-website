70ce3bfa9d2a1d5bece884a1dacf031075979763
dscmari <marian@noekel@gmail.com>
2024-09-09 11:57:30 +0200

    finish sliding burger menu and fix font import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "/src/css/navigation.css";
 
 export default function Navbar() {
+  const [menu, setMenu] = useState(false);
+  const [burgermenu, setBurgermenu] = useState(false);
 
-    
-  return (
-    <nav>
-      <div className="logo-burger-container">
+  const handleResize = () => {
+    setBurgermenu(window.innerWidth < 768)
+  }
+
+  useEffect (() => {
+    window.addEventListener("resize", handleResize);
+    handleResize()
+    console.log("burgermenu: " + burgermenu)
+  }, [])
+
+
+  const toggleMenu = () => {
+    console.log("toggleMenu called");
+    setMenu(!menu)
+  };
+
+  if (burgermenu) {
+    return (
+      <nav>
+        <div className="logo-burger-container">
+          <NavLink to="/">
+            <div className="logo">
+              <img src="./src/assets/react.svg" alt="" />
+            </div>
+          </NavLink>
+          <div id="burger-menu" onClick={toggleMenu}>
+            <span></span>
+          </div>
+        </div>
+        <div className={`items-container burgermenu ${menu ? 'menu-open' : 'menu-closed'}`}>
+          <NavLink to="/Psychotherapie">Psychotherapie</NavLink>
+          <NavLink to="/Angebot">Angebot</NavLink>
+          <NavLink to="/UeberMich">Über Mich</NavLink>
+          <NavLink to="/Krise">Krise</NavLink>
+          <NavLink to="/Kontakt">Kontakt</NavLink>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav>
         <NavLink to="/">
           <div className="logo">
             <img src="./src/assets/react.svg" alt="" />
           </div>
         </NavLink>
-        <div id="burger-menu">
-          <span></span>
+        <div className="items-container">
+          <NavLink to="/Psychotherapie">Psychotherapie</NavLink>
+          <NavLink to="/Angebot">Angebot</NavLink>
+          <NavLink to="/UeberMich">Über Mich</NavLink>
+          <NavLink to="/Krise">Krise</NavLink>
+          <NavLink to="/Kontakt">Kontakt</NavLink>
         </div>
-      </div>
-      <div className="items-container">
-        <NavLink to="/Psychotherapie">Psychotherapie</NavLink>
-        <NavLink to="/Angebot">Angebot</NavLink>
-        <NavLink to="/UeberMich">Über Mich</NavLink>
-        <NavLink to="/Krise">Krise</NavLink>
-        <NavLink to="/Kontakt">Kontakt</NavLink>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
