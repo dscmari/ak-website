c27d55f614ef675a8ce5f43ef4806e47c78bc48e
Marian Noekel <marian@MacCarlo.fritz.box>
2024-09-20 18:02:03 +0200

    implement tailwind in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import '../css/footer.css'
 
 export default function Footer() {
   return (
-    <div className='footer'>
-        <p>@2024</p>
+    <div className='p-8 border-t-2'>
+        <p className='text-center'>@2024</p>
     </div>
   )
 }
