f2b0d8dfa24ce0705a523ddff9e887835e46487c
Marian Noekel <marian@MacCarlo.local>
2024-09-20 13:53:31 +0200

    clean

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import '../css/footer.css'
 
 export default function Footer() {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "/src/css/navigation.css";
 import useScrollPosition from "../utils/useScrollPosition";
